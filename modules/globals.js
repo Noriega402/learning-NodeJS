058f16acb3375990e8781a45da93f02769f47f56
Noriega402 <d.noriegachajon@gmail.com>
2023-01-04 20:12:12 -0600

    ver objeto global, variables de entorno, funciones de tiempo y direccion de archivos y carpetas

---
+++ modules/globals.js
@@ -0,0 +1,19 @@
+console.log(global);
+
+// USO DE FUNCIONES DE TIEMPO
+// let i = 0;
+// let intervalo = setInterval(function(){
+//     console.log(i);
+//     if(i === 3){
+//         clearInterval(intervalo);
+//         console.log('Proceso terminado');
+//     }
+//     i++
+// }, 1000);
+
+// setImmediate(function (){
+//     console.log('Activando un proceso inmediato');
+// })
+
+console.log(__dirname); // Ver la ruta de la carpeta
+console.log(__filename); // Ver la ruta del archivo
