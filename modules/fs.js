5ad877f4fd1ef8b30818729a6f9ee58b638291e3
Noriega402 <d.noriegachajon@gmail.com>
2023-01-04 20:10:22 -0600

    escribiendo en archivos de texto

--- modules/fs.js
+++ modules/fs.js
@@ -6,4 +6,15 @@ function read(route, callback){
     });
 }
 
+function write(route, content, callback){
+    fs.writeFile(route, content, function(err){
+        err
+            ? callback('Ocurrio un error al escribir en el archivo: ' + err)
+            : callback('Se pudo escribir correctamente en el archivo')
+    })
+}
+
 read(__dirname + '/test.txt', console.log); //se pasa el console.log como funcion para imprimir el texto del archivo
+
+let writing = "Estoy ingresando texto en el archivo desde Node JS!"
+write(__dirname + '/writeText.txt', writing,console.log)
