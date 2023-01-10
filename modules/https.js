88bab18781ef961215149f71defcea78a7f4963f
Noriega402 <d.noriegachajon@gmail.com>
2023-01-09 18:17:04 -0600

    fixed method end()

--- modules/https.js
+++ modules/https.js
@@ -3,6 +3,7 @@ const http = require('http');
 http.createServer(function(request, response){
     console.log("Nueva Peticion");
     console.log(request.url);
-
-    request.end();
+    response.end();
 }).listen(3000);
+
+console.log("Escuchando el el puerto 3,000");
